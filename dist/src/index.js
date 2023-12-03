46cfbc0b18e65487aaae2f84c002481198616f3a
Killer12344 <hh1716101@gmail.com>
2023-12-03 19:59:14 +0630

    variable

---
+++ dist/src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let age = 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let sales: number = 1500;
+let course: string = 'TypeScript';
+let is_published = true;
+let level;
+
+const helloWorld = (doc: any) => {
+    console.log(doc);
+    
+}
