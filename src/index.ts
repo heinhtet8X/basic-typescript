5c69aaf68a1273504ea54f27054b746f26759015
Killer12344 <hh1716101@gmail.com>
2023-12-05 20:21:25 +0630

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-const me: {
+type User = {
     readonly id: number,
     name: string,
     work?: string,
     age: (date: string) => void
-} = {
+}
+
+// const me: {
+//     readonly id: number,
+//     name: string,
+//     work?: string,
+//     age: (date: string) => void
+// } = {
+//     id: 1,
+//     name: 'HHA',
+//     work: 'DEV',
+//     age: (date: string) => {
+//         console.log(date);
+//     }
+// };
+
+const user: User = {
     id: 1,
     name: 'HHA',
     work: 'DEV',
